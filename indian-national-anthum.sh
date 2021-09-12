60efc8f3898aa646931f6c3637450c02eaf5e18c
Kaushal Soni <kaushal95300@gmail.com>
2021-09-12 12:22:07 +0530

    Uploading program

---
+++ indian-national-anthum.sh
@@ -0,0 +1,63 @@
+clear
+for((i=0;i<2;i++))
+do
+echo " "
+done
+tput setaf 3
+echo "           ||:::::::::::::::::::::::::::"
+echo "           ||:::::::::::::::::::::::::::"
+tput setaf 7
+echo -n "           ||:::::::::::" && tput setaf 4 && echo -n " @-@ " && tput setaf 7 && echo ":::::::::::"
+echo -n "           ||:::::::::::" && tput setaf 4 && echo -n " @-@ " && tput setaf 7 && echo ":::::::::::"
+tput setaf 2
+echo "           ||:::::::::::::::::::::::::::"
+echo "           ||:::::::::::::::::::::::::::"
+tput setaf 9
+for((i=0;i<=10;i++))
+do
+echo "           ||"
+done
+tput setaf 3
+echo "================================================="
+echo ""
+echo ""
+
+tput setaf 2
+echo "Bharat Mata Ki Jaya "
+echo "Vande Matram "
+
+tput setaf 12
+echo ""
+echo "            NATIONAL ANTHEM "
+echo ""
+
+tput setaf 3
+espeak-ng -s 70 "Bhaarat Mata Ki Jai "
+espeak-ng -s 70 "Vandee Matram"
+
+
+echo "Jana Gana Mana Adhinayak Jai he, Bharat Bhagya Vidhata"
+espeak-ng -s 70 "Jana-gana-mana-adhinayaka jaya he, Bharata-bhagya-vidhata,"
+
+echo "Punjav Sindh Gujrat Maratha, Dravid Utkal Banga"
+espeak-ng -s 70 "Punjab-Sindh-Gujarat-Maratha, Dravida-Utkala-Banga,"
+
+echo "Vindhaya Himachal Yamuna Ganaga, Uchala Jaldhi Taranga"
+espeak-ng -s 70 "Vindhya-Himachala-Yamuna-Ganga, Uchchala-Jaladhi-taranga"
+
+echo "Tab subh naame jaage, Tab Subh Aashish mange, Gaaye tab Jai gaatha"
+espeak-ng -s 70 "Tab shubha naame jaa ge, Tab shubha asisa maan ge, Guy  tab jaya gaa tha,"
+
+echo "Jana Gana Mana Adhinayak Jai he, Bharat Bhagya Vidhata"
+espeak-ng -s 70 "JJana-gana-mana-adhinayaka jaya he, Bharata-bhagya-vidhata," 
+
+echo "Jai He, Jai He, Jai He, Jai Jai Jai, Jai he"
+espeak-ng -s 70 "Jaya he, jaya he, jaya he, Jaya jaya jaya, jaya he!"
+
+tput setaf 6
+echo "Thank you and dont forget to Like, Share and subscribe Devops Gyan"
+espeak-ng -s 100 "Thank you and dont forget to Like, Share and subscribe Devops Gyaaan"
+
+tput setaf 7
+reset
+
